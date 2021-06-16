d0f794364d1d1cc400c0a79a6e9bd91b4b3d08d9
Ahmed Amin <Ahmedaminhassanismail@gmail.com>
2021-06-17 01:31:45 +0200

    initial Commit

---
+++ Get My Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get My Location
+//
+//  Created by Amin  on 6/17/21.
+//  Copyright © 2021 AhmedAmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
